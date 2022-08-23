a3be3fa70af8a9f0d2c692e6a8dbe18ac0d19109
Alex <al.repin.a@yandex.ru>
2022-08-23 21:27:28 +0300

    add array002

---
+++ Array002/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] array = {23, 16, 8, 37, 4, 26, 12, 34, 8};
+
+int n = array.Length;
+int find = 8;
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
